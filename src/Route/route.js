d6ff6c6e23627cdfa264e8aada85eed85dd0e9aa
bharat.karnani6 <bharat.karnani6@gmail.com>
2021-11-21 21:46:21 +0530

    dashboard layout created

--- src/Route/route.js
+++ src/Route/route.js
@@ -4,6 +4,7 @@ import Home from '../Pages/Home/home';
 import Login from '../Pages/Login/login'
 import Dashboard from '../Pages/Dashboard/dashboard';
 import SecuredRoutes from './securedroutes';
+import About from '../Pages/About/about';
 
 
 export default function Routes() {
@@ -13,6 +14,7 @@ export default function Routes() {
             <Switch>
                 <Route exact path="/" ><Home /></Route>
                 <Route exact path="/login" ><Login /></Route>
+                <Route exact path="/about" ><About /></Route>
                 <SecuredRoutes exact path="/dashboard" component={Dashboard} />
             </Switch>
 
