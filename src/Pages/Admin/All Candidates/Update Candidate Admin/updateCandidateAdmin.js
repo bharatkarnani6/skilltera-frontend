6ebf53046816c8548724f4acdfa1f507dde7012d
Bharat Karnani <bharat.karnani6@gmail.com>
2022-01-05 22:41:18 +0530

    inputfield corrected

--- src/Pages/Admin/All Candidates/Update Candidate Admin/updateCandidateAdmin.js
+++ src/Pages/Admin/All Candidates/Update Candidate Admin/updateCandidateAdmin.js
@@ -1,22 +1,193 @@
-import react from 'react'
+import react, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import ApiConstants from "../../../../Services/apiconstants";
+import '../Update Candidate Admin/updateCandidateAdmin.css'
 
-export default function UpdateCandidateAdmin() {
+export default function UpdateCandidateAdmin(props) {
+    const [candidateDataById, setCandidateDataById] = useState({});
+
+    const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
+
+    const candidateData = () => {
+        axios.post(ApiConstants.CANDIDATE_DATA_BY_ID, { id: props.candidateId }).then((response) => {
+
+            // console.log(response);
+            // setCandidateDataById(response.data.candidate)
+
+            setValue("fullname", response.data.candidate.fullname)
+            setValue("country", response.data.candidate.country)
+            setValue("currentCity", response.data.candidate.currentCity)
+            setValue("currentCompany", response.data.candidate.currentCompany)
+            setValue("currentRole", response.data.candidate.currentRole)
+            setValue("email", response.data.candidate.email)
+            setValue("expectedRateC2CorC2H", response.data.candidate.expectedRateC2CorC2H)
+            setValue("experience", response.data.candidate.experience)
+            setValue("experienceDescription", response.data.candidate.experienceDescription)
+            setValue("interestedRole", response.data.candidate.interestedRole)
+            setValue("knownTechnologies", response.data.candidate.knownTechnologies)
+            setValue("linkedInUrl", response.data.candidate.linkedInUrl)
+            setValue("needVisaSponsorship", response.data.candidate.needVisaSponsorship)
+            setValue("phone", response.data.candidate.phone)
+            setValue("relocation", response.data.candidate.relocation)
+            setValue("timeToJoin", response.data.candidate.timeToJoin)
+            setValue("typeOfJob", response.data.candidate.typeOfJob)
+
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        candidateData();
+    }, [props.candidateId])
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+
+
+
     return (
         <>
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Update Candidate</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div class="form-row">
+                                    <label for="exampleFormControlTextarea1">Full Name</label>
+                                    <input type="text" class="form-control mb-2"
+                                        {...register("fullname")}
+                                    />
+                                </div>
+
+                                <div class="form-row">
+                                    <label for="exampleFormControlTextarea1">Phone Number</label>
+                                    <input type="tel" class="form-control mb-2"
+                                        {...register("phone")}
+                                    />
+                                </div>
+                                <div class="form-row">
+                                    <label for="exampleFormControlTextarea1">Email</label>
+                                    <input type="email" class="form-control mb-2"
+                                        {...register("email")}
+                                    />
+                                </div>
+                                <div class="form-row">
+                                    <label for="exampleFormControlTextarea1">LinkedIn url</label>
+                                    <input type="url" class="form-control mb-2"
+                                        {...register("linkedInUrl")}
+                                    />
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="col">
+                                        <label for="exampleFormControlTextarea1">Country</label>
+                                        <input type="text" class="form-control"
+                                            {...register("country")}
+                                        />
+                                    </div>
+                                    <div class="col">
+                                        <label for="exampleFormControlTextarea1">Current City</label>
+                                        <input type="text" class="form-control"
+                                            {...register("currentCity")}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="form-row mt-2">
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1">Relocate</label>
+                                        <select class="form-control"  {...register("relocation")}  >
+                                            <option value={false} >No</option>
+                                            <option value={true} >Yes</option>
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1">Job Type</label>
+                                        <input type="text" class="form-control"
+                                            {...register("typeOfJob")}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="form-row mt-4">
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1">When can you join(Week)</label>
+                                        <select class="form-control" {...register("timeToJoin")} >
+                                            <option value="1" >1</option>
+                                            <option value="2">2</option>
+                                            <option value="1" >3</option>
+                                            <option value="2">4</option>
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1"> Total Overall Experience</label>
+                                        <input type="number" class="form-control"
+                                            {...register("experience")}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="form-row mt-2">
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1">visa status</label>
+                                        <select class="form-control" {...register("needVisaSponsorship")} >
+                                            <option value={false} >No</option>
+                                            <option value={true}>Yes</option>
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label for="exampleFormControlSelect1">Expected salery</label>
+                                        <input type="number" class="form-control"
+                                            {...register("expectedRateC2CorC2H")}
+                                        />
+                                    </div>
+                                </div>
+
+
+                                <div class="form-row mt-2">
+                                    <label for="exampleFormControlSelect1">Current Company</label>
+                                    <input type="text" class="form-control mb-2"
+                                        {...register("currentCompany")}
+                                    />
+                                </div>
+
+                                <div class="form-row mt-2">
+                                    <label for="exampleFormControlSelect1">Current Role</label>
+                                    <input type="text" class="form-control mb-2"
+                                        {...register("interestedRole")}
+                                    />
+                                </div>
+
+                                <div class="form-row mt-2">
+                                    <label for="exampleFormControlSelect1">Technologies /Tools you are good</label>
+                                    <input type="text" class="form-control mb-2"
+                                        {...register("knownTechnologies")}
+                                    />
+                                </div>
+
+
+                                <div class="form-row mt-2">
+                                    <label for="exampleFormControlTextarea1">Brief Description about your skill</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                        {...register("experienceDescription")}
+                                    />
+                                </div>
+
+                                <div class="modal-footer" >
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </div>
+                            </form>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+
                     </div>
                 </div>
             </div>
--- src/Services/apiconstants.js
+++ src/Services/apiconstants.js
@@ -19,6 +19,7 @@ const ApiConstants = {
     ADMIN_LOGIN: `${baseUrl}/admin/login`,
     ADMIN_COMPANY_SIGNUP: `${baseUrl}/admin/company/signup`,
     ADMIN_CANDIDATE_SIGNUP: `${baseUrl}/admin/candidate/signup`,
+    CANDIDATE_DATA_BY_ID: `${baseUrl}/candidate`,
 
 
 }
