82bca7a9b7afe8171ec10e785e1e6603b8eb7e18
Bharat Karnani <bharat.karnani6@gmail.com>
2022-01-26 15:58:55 +0530

    home page fix

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -9,16 +9,12 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-
-      <div class="container-fluid w-100">
-        <div
-          class="banner w-100  banner1"
-          style={{ backgroundColor: "#dddddd" }}
-        >
-          <div class="row">
-            <div class="col">
-              <div class="for-companies fonts">
-                <h3 style={{ fontWeight: "800" }}>For Companies</h3>
+      <div className="home-main">
+        <div className="for-companies">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="for-companies-block fonts">
+                <h3 style={{ fontWeight: "900" }}>For Companies</h3>
                 <p>
                   There are many portals where you can get a list of candidates
                   and job portals to list your jobs with a hope that right
@@ -27,28 +23,52 @@ const Home = () => {
                   talent pipeline for in demand skills, then please try
                   Skilltera and let us know how we can help better?
                 </p>
-                <Link to="/company_login">
-                  {" "}
-                  <button class="btn btn-primary ">Sign Up</button>
-                </Link>
+                <Link to="/company_login"><button className="btn btn-primary ">Sign Up</button></Link>
+              </div>
+
+            </div>
+            <div className="col-lg-6">
+              <div className="banner-image">
+                <img src={Banner} alt="banner" className="img-fluid" />
               </div>
             </div>
-            <div class="col-12 col-md-6 banner-image-section bannerImage1">
-              <div class="banner-image">
-                <img src={Banner} alt="banner" class="img-fluid" />
+          </div>
+        </div>
+        <div className="for-developers">
+          <div className="row">
+            <div className="col-lg-6 order-2 order-xl-1">
+              <div className="banner-image-2">
+                <img src={Banner1} alt="banner" className="img-fluid" />
+              </div>
+            </div>
+            <div className="col-lg-6 order-1 order-xl-2 ">
+              <div className="for-developers-block fonts">
+                <h2 style={{ fontWeight: "900" }}>For Developers</h2>
+                <p>
+                  Our highly experienced founders have felt that putting your
+                  profile in known portals or applying for jobs does not really
+                  yeild expected results. Profile is more than just a resume'.
+                  Building trust around your profile and experience is the key
+                  to get the attention of potential employers. We are about to
+                  launch some features that are not offered by any other job
+                  portal including the likes of LinkedIn. Try Skilltera and
+                  explore the upcoming features that will be shared first with
+                  those who join us by March 2022
+                </p>
+                <Link to="/login"><button className="btn btn-primary  h-20 ">Sign Up</button></Link>
               </div>
             </div>
           </div>
         </div>
 
-        <div
+        {/* <div
           class=" banner  banner2"
           style={{ backgroundColor: "rgb(196 197 194)" }}
         >
           <div class="row">
             <div class="col-md-5 banner-image-section bannerImage2">
               <div class="banner-image  bg-transparent">
-                <img src={Banner1} alt="banner" height="468" width="500" />
+                <img src={Banner1} className="img-fluid" alt="banner" height="468" width="500" />
               </div>
             </div>
             <div class="col-md-6">
@@ -72,9 +92,19 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </div>
+
+
+          <div class="col">
+              <div class="banner-image">
+                <img src={Banner} alt="banner" class="img-fluid" />
+              </div>
+            </div>
+
+
+
+        </div> */}
       </div>
-    </div>
+    </div >
   );
 };
 
