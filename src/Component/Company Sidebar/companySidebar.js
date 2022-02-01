ecce3271b8a3b7b9fd2f14111ed36976f85b8c80
rak <rakeshbaghel9621@gmail.com>
2022-02-01 18:58:43 +0530

    companay dashboard channging

--- src/Component/Company Sidebar/companySidebar.js
+++ src/Component/Company Sidebar/companySidebar.js
@@ -1,72 +1,129 @@
-import react, { useState, createContext, useContext } from 'react'
-import './companySidebar.css'
+import react, { useState, createContext, useContext } from "react";
+import "./companySidebar.css";
 import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
 } from "react-pro-sidebar";
-import 'react-pro-sidebar/dist/css/styles.css';
+import "react-pro-sidebar/dist/css/styles.css";
 import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import {
+  FiHome,
+  FiLogOut,
+  FiArrowLeftCircle,
+  FiArrowRightCircle,
+} from "react-icons/fi";
 import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { useDispatch } from 'react-redux';
-import { SidebarMenuSelectionAction, ToggleAction } from '../../Redux/Action/toggleAction';
-
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useDispatch } from "react-redux";
+import {
+  SidebarMenuSelectionAction,
+  ToggleAction,
+} from "../../Redux/Action/toggleAction";
 
 export default function CompanySidebar() {
+  const dispatch = useDispatch();
+  const [menuCollapse, setMenuCollapse] = useState(false);
 
-    const dispatch = useDispatch();
-    const [menuCollapse, setMenuCollapse] = useState(false)
+  const menuIconClick = () => {
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+    dispatch(ToggleAction(menuCollapse));
+  };
+  const menuSelection = (data) => {
+    dispatch(SidebarMenuSelectionAction(data));
+  };
 
-    const menuIconClick = () => {
-        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-        dispatch(ToggleAction(menuCollapse))
-    };
-    const menuSelection = (data) => {
-        dispatch(SidebarMenuSelectionAction(data))
-    }
+  const logout = () => {
+    localStorage.clear();
+    window.location.pathname = "/";
+  };
 
-    const logout = () => {
-        localStorage.clear();
-        window.location.pathname = "/";
-    }
+  return (
+    <div>
+      <button
+        class="btn btn-primary border border-dark  sidebarBtn"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasWithBothOptions"
+        aria-controls="offcanvasWithBothOptions"
+      >
+        <GiHamburgerMenu />
+      </button>
 
-    return (
-        <div>
-            <ProSidebar collapsed={menuCollapse} width="14rem" style={{ 'height': '87vh' }}>
-                <SidebarHeader>
-                    {/* <div className="logotext">
-                        <h5>Skilltera</h5>
-                    </div> */}
-                    <div className="closemenu d-flex justify-content-center" onClick={menuIconClick}>
-                        {menuCollapse ? (
-                            <GiHamburgerMenu size="2rem" />
-                        ) : (
-                            <GiHamburgerMenu size="2rem" />
-                        )}
-                    </div>
-                </SidebarHeader>
-                <SidebarContent>
-                    <Menu iconShape="square">
-                        <MenuItem active={true} icon={<FiHome />} onClick={() => menuSelection('Dashboard')}>
-                            Dashboard
-                        </MenuItem>
-                        <MenuItem icon={<FaList />} onClick={() => menuSelection('Jobs')}>Jobs</MenuItem>
-                        <MenuItem icon={<BiCog />} onClick={() => menuSelection('Settings')}>Settings</MenuItem>
-                    </Menu>
-                </SidebarContent>
-                <SidebarFooter>
-                    <Menu iconShape="square">
-                        <MenuItem icon={<FiLogOut />} onClick={logout}>Logout</MenuItem>
-                    </Menu>
-                </SidebarFooter>
-            </ProSidebar>
+      <div
+        class="offcanvas offcanvas-start canvas collapseOnSelect"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+        data-trap-focus="true"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+            Dashboard Item
+          </h5>
+          <button
+            type="button"
+            class="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
-    )
+        <div class="offcanvas-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <button
+                class="btn btn-primary  mr-1"
+                active={true}
+                data-bs-dismiss="offcanvas"
+                onClick={() => menuSelection("Dashboard")}
+              >
+                <FiHome />
+              </button>
+              <span> Dashboard</span>
+            </li>
+
+            <li class="list-group-item">
+              <button
+                active={true}
+                class="btn btn-primary mr-1"
+                data-bs-dismiss="offcanvas"
+                onClick={() => menuSelection("Jobs")}
+              >
+                <FaList />
+              </button>
+              <span> Jobs</span>
+            </li>
+            <li class="list-group-item">
+              <button
+                active={true}
+                class="btn btn-primary mr-1"
+                data-bs-dismiss="offcanvas"
+                onClick={() => menuSelection("Settings")}
+              >
+                <BiCog />
+              </button>
+              <span>Setting</span>
+            </li>
+
+            <li class="list-group-item">
+              <button
+                class="btn btn-primary mr-1"
+                active={true}
+                data-bs-dismiss="offcanvas"
+                onClick={() => menuSelection("logout")}
+              >
+                <FiLogOut />
+              </button>
+              logout
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
