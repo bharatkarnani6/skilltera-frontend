83320d5333417b3a2c39da1aa2d3a301a2bb2f92
rak <rakeshbaghel9621@gmail.com>
2022-02-09 21:42:39 +0530

    error message in forget password

--- src/Pages/Forget Password/forgetpassword.js
+++ src/Pages/Forget Password/forgetpassword.js
@@ -130,7 +130,7 @@ export default function ForgetPassword() {
                     pattern: {
                       value:
                         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                      message: "Entered value does not match email format",
+                      message: "Invalid email format",
                     },
                   })}
                   type="email"
