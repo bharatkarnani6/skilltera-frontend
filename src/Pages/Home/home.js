506a21eb8151eaeff3b2029ad88532187601be8e
Bharat Karnani <bharat.karnani6@gmail.com>
2022-02-21 21:46:01 +0530

    redirect to dashboard fix

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../Component/Navbar/navbar";
 import Banner from "../../Assets/banner.png";
 import Banner1 from "../../Assets/bannerimg.png";
@@ -6,6 +6,22 @@ import Banner1 from "../../Assets/bannerimg.png";
 import "./home.css";
 import { Link } from "react-router-dom";
 const Home = () => {
+
+  const [candidateDashboardActive, setcandidateDashboardActive] = useState(false);
+  const [companyDashboardActive, setcompanyDashboardActive] = useState(false);
+
+  //const candidateData = JSON.parse(localStorage.getItem("candidate_data"));
+  // console.log(candidateData.message);
+  useEffect(() => {
+    if (localStorage.getItem("candidateDashboard") == 'true') {
+      setcandidateDashboardActive(localStorage.getItem('candidateDashboard'));
+    }
+    if (localStorage.getItem("companyDashboard") == 'true') {
+      setcompanyDashboardActive(localStorage.getItem('companyDashboard'));
+    }
+  }, [])
+
+
   return (
     <div>
       <Navbar />
@@ -23,7 +39,7 @@ const Home = () => {
                   talent pipeline for in demand skills, then please try
                   Skilltera and let us know how we can help better?
                 </p>
-                <Link to="/company_login"><button className="btn btn-primary btn-lg">Sign Up</button></Link>
+                <Link to={companyDashboardActive ? "/companyDashboard" : "/company_login"}><button className="btn btn-primary btn-lg">Sign-up / Sign-in</button></Link>
               </div>
 
             </div>
@@ -55,54 +71,11 @@ const Home = () => {
                   explore the upcoming features that will be shared first with
                   those who join us by March 2022
                 </p>
-                <Link to="/login"><button className="btn btn-primary btn-lg">Sign Up</button></Link>
+                <Link to={candidateDashboardActive ? "/dashboard" : "/login"}><button className="btn btn-primary btn-lg">Sign-up / Sign-in</button></Link>
               </div>
             </div>
           </div>
         </div>
-
-        {/* <div
-          class=" banner  banner2"
-          style={{ backgroundColor: "rgb(196 197 194)" }}
-        >
-          <div class="row">
-            <div class="col-md-5 banner-image-section bannerImage2">
-              <div class="banner-image  bg-transparent">
-                <img src={Banner1} className="img-fluid" alt="banner" height="468" width="500" />
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="for-developers fonts">
-                <h3 style={{ fontWeight: "800" }}>For Developers</h3>
-                <p>
-                  Our highly experienced founders have felt that putting your
-                  profile in known portals or applying for jobs does not really
-                  yeild expected results. Profile is more than just a resume'.
-                  Building trust around your profile and experience is the key
-                  to get the attention of potential employers. We are about to
-                  launch some features that are not offered by any other job
-                  portal including the likes of LinkedIn. Try Skilltera and
-                  explore the upcoming features that will be shared first with
-                  those who join us by March 2022
-                </p>
-                <Link to="/login">
-                  {" "}
-                  <button class="btn btn-primary  h-20 ">Sign Up</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="col">
-              <div class="banner-image">
-                <img src={Banner} alt="banner" class="img-fluid" />
-              </div>
-            </div>
-
-
-
-        </div> */}
       </div>
     </div >
   );
