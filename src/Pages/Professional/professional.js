821788cfc0c8123cbdee60ded67d149fe894c4fd
rak <rakeshbaghel9621@gmail.com>
2022-03-16 15:43:43 +0530

    fine

--- src/Pages/Professional/professional.js
+++ src/Pages/Professional/professional.js
@@ -365,6 +365,24 @@ const Profile = () => {
                   );
                 }}
                 placeholder={"Write your experience here ..."}
+                toolbar={{
+                  options: [
+                    "inline",
+                    "blockType",
+                    "fontSize",
+                    "list",
+                    "textAlign",
+                    "history",
+                    "embedded",
+                    "emoji",
+                    "image",
+                  ],
+                  inline: { inDropdown: true },
+                  list: { inDropdown: true },
+                  textAlign: { inDropdown: true },
+                  link: { inDropdown: true },
+                  history: { inDropdown: true },
+                }}
               />
             </div>
 
