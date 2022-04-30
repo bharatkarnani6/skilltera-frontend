dd99b5da7e11951625d85e543c27e40c32c54a69
Bharat Karnani <bharat.karnani6@gmail.com>
2022-04-30 21:18:25 +0530

    for changing api

--- src/Services/apiconstants.js
+++ src/Services/apiconstants.js
@@ -1,55 +1,55 @@
-//var baseUrl = "https://skilltera-api.herokuapp.com"
-var baseUrl
-if (process.env.NODE_ENV == "production") {
-  baseUrl = "https://skilltera-api.herokuapp.com";
-} else {
-  baseUrl = "http://localhost:8080";
-}
-console.log(baseUrl);
+// var baseUrl
+// if (process.env.NODE_ENV == "production") {
+//   baseUrl = "https://skilltera-api.herokuapp.com";
+// } else {
+//   baseUrl = "http://localhost:8080";
+// }
+console.log(process.env.PORT);
+console.log(process.env);
 const ApiConstants = {
-  LOGIN: `${baseUrl}/login`,
-  SIGNUP: `${baseUrl}/signup`,
-  RESET_PASSWORD: `${baseUrl}/reset-password`,
-  FORGET_PASSWORD: `${baseUrl}/forgetPassword`,
-  VERIFY_OTP: `${baseUrl}/verifyOtp`,
-  CONTACT_MAIL: `${baseUrl}/contact`,
-  COMPANY_LOGIN: `${baseUrl}/company/login`,
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  RESET_PASSWORD: '/reset-password',
+  FORGET_PASSWORD: '/forgetPassword',
+  VERIFY_OTP: '/verifyOtp',
+  CONTACT_MAIL: '/contact',
+  COMPANY_LOGIN: '/company/login',
 
-  CANDIDATE_DATA: `${baseUrl}/candidates/viewAll`,
+  CANDIDATE_DATA: '/candidates/viewAll',
 
-  COMPANY_DATA: `${baseUrl}/company/viewAll`,
+  COMPANY_DATA: '/company/viewAll',
 
-  ADMIN_LOGIN: `${baseUrl}/admin/login`,
-  ADMIN_COMPANY_SIGNUP: `${baseUrl}/admin/company/signup`,
-  ADMIN_CANDIDATE_SIGNUP: `${baseUrl}/admin/candidate/signup`,
-  CANDIDATE_DATA_BY_ID: `${baseUrl}/candidate`,
-  ADMIN_CANDIDATE_UPDATE: `${baseUrl}/admin/candidate/update`,
-  PROFILE: `${baseUrl}/profile`,
-  ADMIN_COMPANY_PASSWORD_RESET: `${baseUrl}/admin/company/reset`,
+  ADMIN_LOGIN: '/admin/login',
+  ADMIN_COMPANY_SIGNUP: '/admin/company/signup',
+  ADMIN_CANDIDATE_SIGNUP: '/admin/candidate/signup',
+  CANDIDATE_DATA_BY_ID: '/candidate',
+  ADMIN_CANDIDATE_UPDATE: '/admin/candidate/update',
+  PROFILE: '/profile',
+  ADMIN_COMPANY_PASSWORD_RESET: '/admin/company/reset',
 
-  SHORTLISTED_CANDIDATE: `${baseUrl}/company/addShortlisted`,
-  REJECTED_CANDIDATE: `${baseUrl}/company/addRejected`,
-  INTERVIEWING_CANDIDATE: `${baseUrl}/company/addInterviewed`,
-  FUTURE_CANDIDATE: `${baseUrl}/company/addSaved`,
+  SHORTLISTED_CANDIDATE: '/company/addShortlisted',
+  REJECTED_CANDIDATE: '/company/addRejected',
+  INTERVIEWING_CANDIDATE: '/company/addInterviewed',
+  FUTURE_CANDIDATE: '/company/addSaved',
 
-  GET_SHORTLISTED_CANDIDATE: `${baseUrl}/company/shortlisted`,
-  GET_REJECTED_CANDIDATE: `${baseUrl}/company/rejected`,
-  GET_INTERVIEWING_CANDIDATE: `${baseUrl}/company/interviewed`,
-  GET_FUTURE_CANDIDATE: `${baseUrl}/company/saved`,
+  GET_SHORTLISTED_CANDIDATE: '/company/shortlisted',
+  GET_REJECTED_CANDIDATE: '/company/rejected',
+  GET_INTERVIEWING_CANDIDATE: '/company/interviewed',
+  GET_FUTURE_CANDIDATE: '/company/saved',
 
-  CANDIDATE_SHORTLISTED_DATA_BY_COMPANY: `${baseUrl}/candidates/shortlisted`,
-  CANDIDATE_REJECTED_DATA_BY_COMPANY: `${baseUrl}/candidates/rejected`,
-  CANDIDATE_INTERVIEWED_DATA_BY_COMPANY: `${baseUrl}/candidates/interviewed`,
-  CANDIDATE_SELECTED_DATA_BY_COMPANY: `${baseUrl}/candidates/selected`,
-  CANDIDATE_SAVED_DATA_BY_COMPANY: `${baseUrl}/candidates/saved`,
+  CANDIDATE_SHORTLISTED_DATA_BY_COMPANY: '/candidates/shortlisted',
+  CANDIDATE_REJECTED_DATA_BY_COMPANY: '/candidates/rejected',
+  CANDIDATE_INTERVIEWED_DATA_BY_COMPANY: '/candidates/interviewed',
+  CANDIDATE_SELECTED_DATA_BY_COMPANY: '/candidates/selected',
+  CANDIDATE_SAVED_DATA_BY_COMPANY: '/candidates/saved',
 
-  GET_SHORTLISTED_CANDIDATE: `${baseUrl}/company/shortlisted`,
-  GET_REJECTED_CANDIDATE: `${baseUrl}/company/rejected`,
-  GET_INTERVIEWING_CANDIDATE: `${baseUrl}/company/interviewed`,
-  GET_FUTURE_CANDIDATE: `${baseUrl}/company/saved`,
-  GET_ALL_COMPANY_CANDIDATE: `${baseUrl}/company/candidates`,
+  GET_SHORTLISTED_CANDIDATE: '/company/shortlisted',
+  GET_REJECTED_CANDIDATE: '/company/rejected',
+  GET_INTERVIEWING_CANDIDATE: '/company/interviewed',
+  GET_FUTURE_CANDIDATE: '/company/saved',
+  GET_ALL_COMPANY_CANDIDATE: '/company/candidates',
 
-  RESUME_UPLOAD: `${baseUrl}/candidate/upload`
+  RESUME_UPLOAD: '/candidate/upload'
 
 };
 export default ApiConstants;
