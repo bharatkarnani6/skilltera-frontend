f9255f940e53498ab612c06c57afda1982969904
rak9621 <rakeshbaghel9621@gmail.com>
2022-05-06 12:29:11 +0530

    favicon and captcha

--- src/Component/Navbar/navbar.js
+++ src/Component/Navbar/navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <NavLink exact to='/' activeClassName="active" activeStyle={{ color: "red" }}><img src={Logo} alt="logo" className="img-fluid" /></NavLink>
+            <NavLink exact to='/' activeClassName="active" activeStyle={{ color: "red" }}><img src={Logo} alt="logo" className="img-fluid logo" /></NavLink>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
