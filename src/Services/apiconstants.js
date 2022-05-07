3672655bb3f23fce130d9aa5c7b59ae901e6bb06
vjagadish1972 <jagadish.velagapudi@gmail.com>
2022-05-07 21:31:55 +0530

    first commit

--- src/Services/apiconstants.js
+++ src/Services/apiconstants.js
@@ -1,55 +1,59 @@
-//var baseUrl = "https://skilltera-api.herokuapp.com"
-var baseUrl
-if (process.env.NODE_ENV == "production") {
-  baseUrl = "https://skilltera-api.herokuapp.com";
-} else {
-  baseUrl = "http://localhost:8080";
-}
-console.log(baseUrl);
+// var baseUrl
+// if (process.env.NODE_ENV == "production") {
+//   baseUrl = "https://skilltera-api.herokuapp.com";
+// } else {
+//   baseUrl = "http://localhost:8080";
+// }
+// console.log(process.env.PORT);
+// console.log(process.env);
+// const baseURL = process.env.NODE_ENV === "production"
+//     ? "/api"
+//     : "http://localhost:5000/api";
+
 const ApiConstants = {
-  LOGIN: `${baseUrl}/login`,
-  SIGNUP: `${baseUrl}/signup`,
-  RESET_PASSWORD: `${baseUrl}/reset-password`,
-  FORGET_PASSWORD: `${baseUrl}/forgetPassword`,
-  VERIFY_OTP: `${baseUrl}/verifyOtp`,
-  CONTACT_MAIL: `${baseUrl}/contact`,
-  COMPANY_LOGIN: `${baseUrl}/company/login`,
+  LOGIN: '/api/login',
+  SIGNUP: '/api/signup',
+  RESET_PASSWORD: '/api/reset-password',
+  FORGET_PASSWORD: '/api/forgetPassword',
+  VERIFY_OTP: '/api/verifyOtp',
+  CONTACT_MAIL: '/api/contact',
+  COMPANY_LOGIN: '/api/company/login',
 
-  CANDIDATE_DATA: `${baseUrl}/candidates/viewAll`,
+  CANDIDATE_DATA: '/api/candidates/viewAll',
 
-  COMPANY_DATA: `${baseUrl}/company/viewAll`,
+  COMPANY_DATA: '/api/company/viewAll',
 
-  ADMIN_LOGIN: `${baseUrl}/admin/login`,
-  ADMIN_COMPANY_SIGNUP: `${baseUrl}/admin/company/signup`,
-  ADMIN_CANDIDATE_SIGNUP: `${baseUrl}/admin/candidate/signup`,
-  CANDIDATE_DATA_BY_ID: `${baseUrl}/candidate`,
-  ADMIN_CANDIDATE_UPDATE: `${baseUrl}/admin/candidate/update`,
-  PROFILE: `${baseUrl}/profile`,
-  ADMIN_COMPANY_PASSWORD_RESET: `${baseUrl}/admin/company/reset`,
+  ADMIN_LOGIN: '/api/admin/login',
+  ADMIN_COMPANY_SIGNUP: '/api/admin/company/signup',
+  ADMIN_CANDIDATE_SIGNUP: '/api/admin/candidate/signup',
+  CANDIDATE_DATA_BY_ID: '/api/candidate',
+  ADMIN_CANDIDATE_UPDATE: '/api/admin/candidate/update',
+  PROFILE: '/api/profile',
+  ADMIN_COMPANY_PASSWORD_RESET: '/api/admin/company/reset',
 
-  SHORTLISTED_CANDIDATE: `${baseUrl}/company/addShortlisted`,
-  REJECTED_CANDIDATE: `${baseUrl}/company/addRejected`,
-  INTERVIEWING_CANDIDATE: `${baseUrl}/company/addInterviewed`,
-  FUTURE_CANDIDATE: `${baseUrl}/company/addSaved`,
+  SHORTLISTED_CANDIDATE: '/api/company/addShortlisted',
+  REJECTED_CANDIDATE: '/api/company/addRejected',
+  INTERVIEWING_CANDIDATE: '/api/company/addInterviewed',
+  FUTURE_CANDIDATE: '/api/company/addSaved',
 
-  GET_SHORTLISTED_CANDIDATE: `${baseUrl}/company/shortlisted`,
-  GET_REJECTED_CANDIDATE: `${baseUrl}/company/rejected`,
-  GET_INTERVIEWING_CANDIDATE: `${baseUrl}/company/interviewed`,
-  GET_FUTURE_CANDIDATE: `${baseUrl}/company/saved`,
+  GET_SHORTLISTED_CANDIDATE: '/api/company/shortlisted',
+  GET_REJECTED_CANDIDATE: '/api/company/rejected',
+  GET_INTERVIEWING_CANDIDATE: '/api/company/interviewed',
+  GET_FUTURE_CANDIDATE: '/api/company/saved',
 
-  CANDIDATE_SHORTLISTED_DATA_BY_COMPANY: `${baseUrl}/candidates/shortlisted`,
-  CANDIDATE_REJECTED_DATA_BY_COMPANY: `${baseUrl}/candidates/rejected`,
-  CANDIDATE_INTERVIEWED_DATA_BY_COMPANY: `${baseUrl}/candidates/interviewed`,
-  CANDIDATE_SELECTED_DATA_BY_COMPANY: `${baseUrl}/candidates/selected`,
-  CANDIDATE_SAVED_DATA_BY_COMPANY: `${baseUrl}/candidates/saved`,
+  CANDIDATE_SHORTLISTED_DATA_BY_COMPANY: '/api/candidates/shortlisted',
+  CANDIDATE_REJECTED_DATA_BY_COMPANY: '/api/candidates/rejected',
+  CANDIDATE_INTERVIEWED_DATA_BY_COMPANY: '/api/candidates/interviewed',
+  CANDIDATE_SELECTED_DATA_BY_COMPANY: '/api/candidates/selected',
+  CANDIDATE_SAVED_DATA_BY_COMPANY: '/api/candidates/saved',
 
-  GET_SHORTLISTED_CANDIDATE: `${baseUrl}/company/shortlisted`,
-  GET_REJECTED_CANDIDATE: `${baseUrl}/company/rejected`,
-  GET_INTERVIEWING_CANDIDATE: `${baseUrl}/company/interviewed`,
-  GET_FUTURE_CANDIDATE: `${baseUrl}/company/saved`,
-  GET_ALL_COMPANY_CANDIDATE: `${baseUrl}/company/candidates`,
+  GET_SHORTLISTED_CANDIDATE: '/api/company/shortlisted',
+  GET_REJECTED_CANDIDATE: '/api/company/rejected',
+  GET_INTERVIEWING_CANDIDATE: '/api/company/interviewed',
+  GET_FUTURE_CANDIDATE: '/api/company/saved',
+  GET_ALL_COMPANY_CANDIDATE: '/api/company/candidates',
 
-  RESUME_UPLOAD: `${baseUrl}/candidate/upload`
+  RESUME_UPLOAD: '/api/candidate/upload'
 
 };
 export default ApiConstants;
